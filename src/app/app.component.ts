8675e92d3046061a667a8512b85cdb4690d77ab8
Michael Zemler <michael.zemler@hotmail.com>
2018-11-23 21:04:24 +0100

    added Service component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   template:
   `<h1>{{title}}</h1>
-   <products></products>`
+   <products></products>`,
+   providers: [ProductService]
 })
 export class AppComponent {
   title = 'second';
