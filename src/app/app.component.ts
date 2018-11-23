e3f2a1149a86eba579c5ccef4bc6d5147da967ad
Michael Zemler <michael.zemler@hotmail.com>
2018-11-23 21:50:45 +0100

    Button style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import { ProductService } from './product.service';
-import { AdvertisementService } from './advertisement.service';
+import { Component } from '@angular/core'
+import { ProductService } from './product.service'
+import { AdvertisementService } from './advertisement.service'
 
 @Component({
   selector: 'app-root',
-  template:
-  `<h1>{{title}}</h1>
+  template:`
+  <button class ="btn btn-primary"
+  [style.backgroundColor]="isActive ? 'blue':'gray'">Submit</button>
+  <h1>{{title}}</h1>
+  <img src="{{imageURL}}" />
    <products></products>
    <advertisement></advertisement>`,
    providers: [ProductService, AdvertisementService]
 })
 export class AppComponent {
+  isActive = true;
   title = 'second';
+  imageURL = 'https://i.redd.it/ipda90h8dk311.jpg'
 }
